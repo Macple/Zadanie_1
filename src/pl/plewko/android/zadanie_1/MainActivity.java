e1ef21014e090bd3cd46508dff8b2f31b9401ea1
Maciej Plewko <maciej.plewko@gmail.com>
2013-12-16 03:14:44 +0100

    [MOD] MainActivity change from Zadanie1 to Zadanie2 [ADD] Login layout at MainActivity: login EditText, LoginButton and Dialog [BUG] Fixed HOME_BUTTON bug

--- src/pl/plewko/android/zadanie_1/MainActivity.java
+++ src/pl/plewko/android/zadanie_1/MainActivity.java
@@ -1,15 +1,72 @@
 package pl.plewko.android.zadanie_1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+/**
+ * @author Maciej Plewko
+ * @since December 6, 2013
+ */
 public class MainActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Get the view from activity_main.xml
-		setContentView(R.layout.activity_main);
-	}
+    final private Context context = this;
+    private Button loginButton;
+    private TextView nameTextField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        nameTextField = (EditText) findViewById(R.id.person_name);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog loginDialog = new Dialog(context);
+                loginDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                loginDialog.setContentView(R.layout.login_dialog);
+
+                String name = "";
+                String message = "";
+
+                try {
+                    name = nameTextField.getText().toString().trim();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+                TextView dialogMessage = (TextView) loginDialog.findViewById(R.id.login_dialog_text);
+                Button dialogButton = (Button) loginDialog.findViewById(R.id.dialog_button_ok);
+
+
+                if (name.length() > 0) {
+                    message = "Hello " + name + "!";
+                } else {
+                    message = "Name field is empty.\nPlease enter your name.";
+                }
+
+                dialogMessage.setText(message);
+
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        loginDialog.dismiss();
+                    }
+                });
+
+                loginDialog.show();
+            }
+        });
+    }
 
 }
--- src/pl/plewko/android/zadanie_1/SplashScreenActivity.java
+++ src/pl/plewko/android/zadanie_1/SplashScreenActivity.java
@@ -6,56 +6,64 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 
+/**
+ * @author Maciej Plewko
+ * @since December 6, 2013
+ */
 public class SplashScreenActivity extends Activity {
 
-	// Set Duration of the Splash Screen
-	private final static long DELAY = 5000;
-	private boolean isBackPressed = false;
-	private Thread splashThread;
+    private final static long DELAY = 5000;
+    private volatile boolean isBackPressed = false;
+    private Thread splashThread;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		setContentView(R.layout.splash_screen);
-		
-		splashThread = new Thread() {
+        setContentView(R.layout.splash_screen);
+
+        splashThread = new Thread() {
             public void run() {
                 try {
                     sleep(DELAY);
                     if (!isBackPressed) {
-                    	
-                    	Log.d("splashThread", "MainActivity is running");
-                    	Intent myIntent = new Intent(SplashScreenActivity.this,
-        						MainActivity.class);
-        				startActivity(myIntent);
 
+                        Log.d("splashThread", "MainActivity is running");
+                        Intent myIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                        startActivity(myIntent);
                     }
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-
-                finally {
-
+                } finally {
                     finish();
-
                 }
             }
 
         };
 
         splashThread.start();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (splashThread.getState() == Thread.State.TIMED_WAITING) {
+            splashThread.interrupt();
+            finish();
+        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+
+        //Log.d("onKeyDown", "BACK key pressed: FINISHING THE APPLICATION");
         isBackPressed = true;
-        Log.d("onKeyDown", "BACK key pressed: FINISHING THE APPLICATION");
+        splashThread.interrupt();
         finish();
     }
 }
